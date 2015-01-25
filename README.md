1f3656a08a38df84daa3ea413d11f25c4db9fed1
matthewjanderson <matt@matt.co.uk>
2015-01-25 12:37:18 +0000

    re-upload following execution

--- README.md
+++ README.md
@@ -1,40 +1,46 @@
-GettingandCleaningData
+<p><markdown>
+#Markdown Live Editor#
+Getting and Cleaning Data
 ================================
 Course Project 2015
 ================================
 
 This is the R script (run_analysis.R) for the Week 3 project in Getting and Cleaning Data Course. 
 
-This script follows the steps outlined in the project aims:
-##Aims
-	1. Merges the training and the test sets to create one data set.
-	2. Extracts only the measurements on the mean and standard deviation for each measurement
-		Means and standard deviations were only included into this set 
-		if the mean or standard was at the end of the variable name, 
-		not with the first part of the variable name.
-	3. Uses descriptive activity names to name the activities in the data set
-	4. Appropriately labels the data set with descriptive variable names. 
-	5. From the data set in step 4, 
-	creates a second, independent tidy data set with the 
-	average of each variable for each activity and each subject.
-	
-The output file (Means.txt) contains means for each subject for each of the activities 
-for each of the means and standard deviation variables of the full dataset 
-(66 variables, with Subject and Activity as IDs). 
+As a prerequisite you will need the **plyr** and **dplyr** packages installed for data manipulation.
+If needed install using;
 
-##Steps for the analysis
-	*Loading Data
-	*Loading Features (to assign Column Names)
-	*Assigning column names to Test and Train using Features
-	*SubsettingData
-	*Loading Subject and Activity Labels
-	*Converting Subject and Activity file format and information
-	*Adding Activity Labels as a new column
-	*Adding subject information as a new column
-	*renaming Subject and Activity columns
-	*Combine Training and Testing data into one table
-	*Using dplyr:
-	*arranged and grouped data by Subject and then Activity
-	*summarised - resulting in a tidy set with the average of each variable (see codebook)
-	for each activity by each subject 
-	*write object 'means' out to a text file
+ - `install.packages("dplyr")` 
+   
+ - `install.packages("plyr")`
+
+You will also need the UCI HAR Dataset, extracted to your working directory.
+
+ - Download this from [here][1]
+
+This script performs the following
+##Aims
+
+ 1. Merges the training and the test sets to create one data set
+ 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+ 3. Uses descriptive activity names to name the activities in the data set
+ 4. Appropriately labels the data set with descriptive variable names.
+ 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+##Steps
+When executed the **run_analysis.r** script performs the following steps;
+
+ 1. Load Data & Features
+ 2. Assign column names
+ 3. Extract the Mean and Standard Deviation
+ 4. Load Subject and Activity Labels
+ 5. Convert Reference files
+ 6. Add Activity Labels as a new column
+ 7. Add subject information as a new column
+ 8. rename Columns
+ 9. Combine Training and Testing data into one table
+ 10. Grouping data by Subject and then Activity
+ 11. Write to "Means.txt" file
+
+  [1]: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+</markdown></p>
--- run_analysis.R
+++ run_analysis.R
@@ -1,56 +1,51 @@
 # run_analysis.r
 #You should create one R script called run_analysis.R that does the following. 
     #1. Merges the training and the test sets to create one data set.
-    #2. Extracts only the measurements on the mean and standard deviation 
-    #   for each measurement. 
+    #2. Extracts only the measurements on the mean and standard deviation for each measurement. 
     #3. Uses descriptive activity names to name the activities in the data set
     #4. Appropriately labels the data set with descriptive variable names. 
-    #5. From the data set in step 4, 
-        #creates a second, independent tidy data set 
-        #with the average of each variable for each activity and each subject.
+    #5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
-##Loading Data##
+##Load Data & Features to assign column names
 
 	Train = read.table("./UCI HAR Dataset/train/X_train.txt") 
 	Test = read.table("./UCI HAR Dataset/test/X_test.txt")
-
-#Loading Features (to assign Column Names)
 	Features = read.table("./UCI HAR Dataset/features.txt", header = FALSE)
 	FeaturesNames <- Features[,2]
-#Assigning column names to Test and Train using Features
+	
+#Assign column names to Test and Train
 	colnames(Train) = FeaturesNames
 	colnames(Test) = FeaturesNames
 
-#SubsettingData
-#Extracts only the measurements on the mean and standard deviation for each measurement.
+#Extract the Mean and Standard deviation 
 	Train_mean_std <- Train[, grep("mean[[:punct:]]|std[[:punct:]]",names(Train))]
 	Test_mean_std <- Test[, grep("mean[[:punct:]]|std[[:punct:]]",names(Test))]
 
-#Loading Subject and Activity Labels
+#Load Subject and Activity Labels
 	TrainingSubjectLabels = read.table("./UCI HAR Dataset/train/subject_train.txt")
 	TestSubjectLabels = read.table("./UCI HAR Dataset/test/subject_test.txt")
 	TrainActivityLabel = read.table("./UCI HAR Dataset/train/y_train.txt") 
 	TestActivityLabel = read.table("./UCI HAR Dataset/test/y_test.txt")
 
-#Converting Reference files
+#Convert Reference files
 	library(plyr)
 	TrainActivityRELabel <- mapvalues(TrainActivityLabel$V1, c(1,2,3,4,5,6), c("WALKING","WALKING_UPSTAIRS","WALKING_DOWNSTAIRS",
                                 "SITTING","STANDING","LAYING"))
 	TestActivityRELabel <- mapvalues(TestActivityLabel$V1, c(1,2,3,4,5,6), c("WALKING","WALKING_UPSTAIRS","WALKING_DOWNSTAIRS",
                                                                            "SITTING","STANDING","LAYING"))
-##Adding Activity Labels as a new column
+##Add Activity Labels as a new column
 	Train_mean_std = cbind(TrainActivityRELabel,Train_mean_std)
 	Test_mean_std  = cbind(TestActivityRELabel,Test_mean_std)
 
-#Adding subject information as a new column
+#Add subject information as a new column
 	Train_mean_std<-cbind(as.integer(TrainingSubjectLabels$"V1"),Train_mean_std)
 	Test_mean_std<-cbind(as.integer(TestSubjectLabels$"V1"),Test_mean_std) 
 
-#renaming columns
+#Rename columns as outlined
 	colnames(Train_mean_std)[1:2] <- c("Subject","Activity")
 	colnames(Test_mean_std)[1:2] <- c("Subject","Activity")
 
-##Combine Training and Testing data into one table##
+##Combine Training and Testing data into one table
 	JoinedData = as.data.frame(rbind(Train_mean_std,Test_mean_std))
  
 #Grouping data by Subject and then Activity
@@ -61,7 +56,6 @@
         summarise_each(funs(mean))
 	Means
 	write.table(Means, file = "Means.txt", row.names = FALSE)
-#Please upload the tidy data set created in step 5 of the instructions. 
-#Please upload your data set as a txt file created with write.table() using row.name=FALSE 
+ 
 
 
